cbc7152e23ffcf91ad971b063a620f9aa98477fd
bhuone23o33 <128846581+bhuone23o33@users.noreply.github.com>
2023-04-20 11:56:01 +0530

    Update About.jsx

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -13,20 +13,13 @@ function About() {
                 React Front To Back
                 </a>{' '}
                 Udemy course by
-                <strong>
-                <a href='https://traversymedia.com'> Brad Traversy</a>
-                </strong>
+                
                 .
             </p>
             <p className='text-lg text-gray-400'>
                 Version <span className='text-white'>1.0.0</span>
             </p>
-            <p className='text-lg text-gray-400'>
-                Layout By:
-                <a className='text-white' href='https://twitter.com/hassibmoddasser'>
-                Hassib Moddasser
-                </a>
-            </p>
+            
         </div>
     </div>
   )
