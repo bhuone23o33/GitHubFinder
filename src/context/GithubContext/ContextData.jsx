38907f0600a6892b7d43d6c9a01adc9a5865288f
Bhuvan <bhuonepartapsingh@gmail.com>
2023-10-26 13:57:51 +0530

    some changes

--- src/context/GithubContext/ContextData.jsx
+++ src/context/GithubContext/ContextData.jsx
@@ -3,35 +3,35 @@ import { createContext } from 'react'
 import { useReducer } from 'react';
 import GitUser from './ReducerData';
 
-const Finder  = createContext();
+const Finder = createContext();
 // initializing keys or url
 const GITHUB_URL = import.meta.env.VITE_GITHUB_URL;
 const GITHUB_TOKEN = import.meta.env.VITE_GITHUB_TOKEN;
 
 
-export const FinderProvider = ({ children })=>{
+export const FinderProvider = ({ children }) => {
 
   // const [isLoading,setisLoading] = useState(true);
   // const [user,setuser] = useState([]);
 
   // intializing reducer for initial state just like state
   const oldUser = {
-    users:[],
-    user:{},
-    userRepos:[],
-    isLoading:false
+    users: [],
+    user: {},
+    userRepos: [],
+    isLoading: false
   }
 
   // using Reducers state->to use data and dispatch->to update data just like we use states ok!!
-  const [state,dispatch] = useReducer(GitUser,oldUser);
-  
+  const [state, dispatch] = useReducer(GitUser, oldUser);
+
   // fetching data from github(only for testing)
-  const fetchData = async ()=>{
+  const fetchData = async () => {
     setLoading()
-    const res = await fetch(`${GITHUB_URL}/users`,{
-        headers:{
-            Authorization:`token ${GITHUB_TOKEN}`
-        }
+    const res = await fetch(`${GITHUB_URL}/users`, {
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`
+      }
     })
     const data = await res.json();
 
@@ -42,64 +42,64 @@ export const FinderProvider = ({ children })=>{
     // instead of this 
     dispatch({
       // sending type so it identify it
-      type:'GIT_USER',
-      data:data
+      type: 'GIT_USER',
+      data: data
     }
     );
   }
 
 
   // get singleuser from github
-  const getUser = async(user)=>{
+  const getUser = async (user) => {
     setLoading()
-    const res = await fetch(`${GITHUB_URL}/users/${user}`,{
-      headers:{
-        Authorization:`token ${GITHUB_TOKEN}`
+    const res = await fetch(`${GITHUB_URL}/users/${user}`, {
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`
       }
     });
     const data = await res.json();
     console.log(data);
     // check if user not found
-    if(res.status === 404){
-      window.location='/notfound';
-    }else{
+    if (res.status === 404) {
+      window.location = '/notfound';
+    } else {
       dispatch({
-        type:'SET_USER',
-        payload:data
+        type: 'SET_USER',
+        payload: data
       })
     }
   }
 
   // getting only 10 repos using param
   const params = new URLSearchParams({
-    sort:'created',
-    per_page:10,
+    sort: 'created',
+    per_page: 10,
   })
 
   // getting repos of the user
-  const getRepos = async(user)=>{
+  const getRepos = async (user) => {
     setLoading()
-    const res = await fetch(`${GITHUB_URL}/users/${user}/repos?${params}`,{
-      headers:{
-        Authorization:`token ${GITHUB_TOKEN}`
+    const res = await fetch(`${GITHUB_URL}/users/${user}/repos?${params}`, {
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`
       }
     });
     const data = await res.json();
     // check if user not found
-    if(res.status === 404){
-      window.location='/notfound';
-    }else{
+    if (res.status === 404) {
+      window.location = '/notfound';
+    } else {
       dispatch({
-        type:'SET_REPOS',
-        payload:data
+        type: 'SET_REPOS',
+        payload: data
       })
     }
   }
 
 
   // setLoading
-  const setLoading=()=>{
-    dispatch({type:'SET_LOADING'})
+  const setLoading = () => {
+    dispatch({ type: 'SET_LOADING' })
   }
 
 
--- src/context/GithubContext/GithubActions.js
+++ src/context/GithubContext/GithubActions.js
@@ -1,49 +1,59 @@
 // initializing keys or url
 const GITHUB_URL = import.meta.env.VITE_GITHUB_URL;
-// const GITHUB_TOKEN = import.meta.env.VITE_GITHUB_TOKEN;
+const GITHUB_TOKEN = import.meta.env.VITE_GITHUB_TOKEN;
 
 
 // searching Users
-export const SearchUsers = async(user)=>{
-    const res = await fetch(`${GITHUB_URL}/search/users?q=${user}`);
-    // const res = await fetch(`${GITHUB_URL}/search/users?q=${user}`,{
-    //   headers:{
-    //     Authorization:`token ${GITHUB_TOKEN}`
-    //   }
-    // });
-    // const res  = await fetch(`https://api.github.com/search/users?q=${user}`)
-    const {items} = await res.json();
-    return items;
+export const SearchUsers = async (user) => {
+  // const res = await fetch(`${GITHUB_URL}/search/users?q=${user}`);
+  const res = await fetch(`${GITHUB_URL}/search/users?q=${user}`, {
+    headers: {
+      Authorization: `token ${GITHUB_TOKEN}`
+    }
+  });
+  // const res  = await fetch(`https://api.github.com/search/users?q=${user}`)
+  const { items } = await res.json();
+  return items;
 }
 
 // get singleuser from github
-export const getUser = async(user)=>{
-    const res = await fetch(`${GITHUB_URL}/users/${user}`);
-
-    const data = await res.json();
-    // check if user not found
-    if(res.status === 404){
-      window.location='/notfound';
-    }else{
-      return data;
+export const getUser = async (user) => {
+  // const res = await fetch(`${GITHUB_URL}/users/${user}`);
+  const res = await fetch(`${GITHUB_URL}/users/${user}`, {
+    headers: {
+      Authorization: `token ${GITHUB_TOKEN}`
     }
+  });
+
+  const data = await res.json();
+  // check if user not found
+  if (res.status === 404) {
+    window.location = '/notfound';
+  } else {
+    return data;
+  }
 }
 
 
-  // getting only 10 repos using param
-  const params = new URLSearchParams({
-    sort:'created',
-    per_page:10,
-  })
+// getting only 10 repos using param
+const params = new URLSearchParams({
+  sort: 'created',
+  per_page: 10,
+})
 
-  // getting repos of the user
-export const getRepos = async(user)=>{
-    const res = await fetch(`${GITHUB_URL}/users/${user}/repos?${params}`);
-    const data = await res.json();
-    // check if user not found
-    if(res.status === 404){
-      window.location='/notfound';
-    }else{
-      return data;
+// getting repos of the user
+export const getRepos = async (user) => {
+  // const res = await fetch(`${GITHUB_URL}/users/${user}/repos?${params}`);
+  const res = await fetch(`${GITHUB_URL}/users/${user}/repos?${params}`, {
+    headers: {
+      Authorization: `token ${GITHUB_TOKEN}`
     }
+  });
+  const data = await res.json();
+  // check if user not found
+  if (res.status === 404) {
+    window.location = '/notfound';
+  } else {
+    return data;
+  }
 }
